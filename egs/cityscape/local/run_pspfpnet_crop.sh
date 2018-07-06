f1df6274bae738364161c50c95aa150228561686
YiwenShaoStephen <sywcs007wow@gmail.com>
2018-07-06 16:06:47 -0400

    clean up

--- egs/cityscape/local/run_pspfpnet_crop.sh
+++ egs/cityscape/local/run_pspfpnet_crop.sh
@@ -3,24 +3,21 @@ stage=0
 
 num_colors=3
 num_classes=9
-train_image_size=384
+train_image_size=768
 
-epochs=200
+epochs=400
 batch_size=16
 lr=0.01
 momentum=0.9
-dir=exp/crop/pspfpnet50_alpha10
+dir=exp/crop/pspfpnet101_alpha20
 
-. ./cmd.sh
 . ./path.sh
-. ./scripts/parse_options.sh
-
-
+. ./utils/parse_options.sh
 
 
 if [ $stage -le 1 ]; then
   mkdir -p $dir/configs
-  echo "$0: Creating core configuration and unet configuration"
+  echo "$0: Creating core configuration"
 
   cat <<EOF > $dir/configs/core.config
   num_classes $num_classes
@@ -32,19 +29,20 @@ fi
 
 if [ $stage -le 2 ]; then
   echo "$0: Training the network....."
-  $cmd --gpu 1 --mem 20G $dir/train.log limit_num_gpus.sh local/train_crop.py \
+  python3 local/train_iou.py \
        --batch-size $batch_size \
        --momentum $momentum \
        --train-image-size $train_image_size \
        --epochs $epochs \
        --lr $lr \
        --loss bce \
-       --alpha 10 \
+       --alpha 20 \
        --arch pspfpnet \
        --log-freq 100 \
        --core-config $dir/configs/core.config \
        --visualize \
        --pretrain \
        --tensorboard \
+       --crop \
        $dir
 fi
